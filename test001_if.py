58b459fc9487f2341e84d13f217d9a4a9d4cc3b8
rkbapi459 <rkbapi459@gmail.com>
2020-10-05 23:09:14 +0100

    first python script

---
+++ test001_if.py
@@ -0,0 +1,20 @@
+if True:
+    print('true')
+    print('ranjan')
+else:
+    print('false')
+    print('pini')
+
+print('*'*50)
+
+print(0 % 2)
+l = 0 % 2
+print('l -> ', l)
+if l == 0:
+    print('ranjan')
+else:
+    print('behera')
+
+print('*'*50)
+
+if l == 0:print('ranjan')
